d97b7d325c3033131b7a67eb03a15877b1bb71b1
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-21 09:57:52 +0300

    feat: Add movie detail page cells

---
+++ Movielium/Views/Cells/Movie Detail Screen/ImageMovieDetailCell.swift
@@ -0,0 +1,39 @@
+//
+//  ImageMovieDetailCell.swift
+//  Movielium
+//
+//  Created by Nazmi Yavuz on 21.04.2022.
+//  Copyright © 2022 Nazmi Yavuz. All rights reserved.
+//
+
+import UIKit
+
+class ImageMovieDetailCell: UITableViewCell {
+    
+    // MARK: - Views
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    // first loading func
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    // MARK: - Services
+    
+    // MARK: - Private Functions
+    
+    // MARK: - Action
+    
+}
+
+extension ImageMovieDetailCell: NibReusable {}
